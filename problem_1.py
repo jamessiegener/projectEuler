c4a84a70bfcd3cef0561678386f76b94dc698c91
jamessiegener <143543186+jamessiegener@users.noreply.github.com>
2024-06-13 15:22:20 -0400

    Commented Problem_1

--- problem_1.py
+++ problem_1.py
@@ -2,10 +2,10 @@
 
 # https://projecteuler.net/problem=1
 
-sum=0
+sum=0 # Initialize our sum variable to 0
 
-for i in range(1,1000):
-    if i%3==0 or i%5==0:        
-        sum+=i
+for i in range(1,1000): # Count through numbers 1 to 999
+    if i%3==0 or i%5==0:        ## Determine if they are divisible by 3 or 5
+        sum+=i # If so, add 1 to our sum, else do nothing
 
-print sum
+print sum # Print our answer
